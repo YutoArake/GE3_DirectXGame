4c6bb8e865dc6254ebb705bffb9f512865a7a190
YutoArake <k021g1420@g.neec.ac.jp>
2022-10-17 14:12:46 +0900

    02-02までの処理

--- Input.h
+++ Input.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <windows.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
 
 class Input {
 public:
+	// namespace省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+public: // メンバ関数
 	/// <summary>
 	/// 初期化
 	/// </summary>
@@ -12,4 +19,8 @@ public:
 	/// 更新
 	/// </summary>
 	void Update();
+
+private: // メンバ変数
+	// キーボードデバイス
+	ComPtr<IDirectInputDevice8> keyboard;
 };
