e254fed1be846f68b54cb0eaa1a089df802bae8d
YutoArake <k021g1420@g.neec.ac.jp>
2022-10-17 13:42:40 +0900

    02-01までの処理

---
+++ Input.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <windows.h>
+
+class Input {
+public:
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize(HINSTANCE hInstance, HWND hwnd);
+
+	/// <summary>
+	/// XV
+	/// </summary>
+	void Update();
+};
